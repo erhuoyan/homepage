6120646c63fbfbbf198b302a00cd623e4fd440fc
erhuoyan <2949702406@qq.com>
2024-08-01 14:20:21 +0800

    fitst commit

---
+++ assets/js/main.js
@@ -0,0 +1,127 @@
+var iUp = (function () {
+	var time = 0,
+		duration = 150,
+		clean = function () {
+			time = 0;
+		},
+		up = function (element) {
+			setTimeout(function () {
+				element.classList.add("up");
+			}, time);
+			time += duration;
+		},
+		down = function (element) {
+			element.classList.remove("up");
+		},
+		toggle = function (element) {
+			setTimeout(function () {
+				element.classList.toggle("up");
+			}, time);
+			time += duration;
+		};
+	return {
+		clean: clean,
+		up: up,
+		down: down,
+		toggle: toggle
+	};
+})();
+
+function getBingImages() {
+	/**
+	 * 获取Bing壁纸
+	 * 先使用 GitHub Action 每天获取 Bing 壁纸 URL 并更新 images.json 文件
+	 * 然后读取 images.json 文件中的数据
+	 */
+	
+	/**
+		修改为从本地加载图片
+		图片通过python脚本每天定时获取保存到本地
+		图片在获取后进行了压缩以保证浏览器加载速度
+		具体见getimage.py
+	*/
+
+	var indexName = "bing-image-index";
+	var index = sessionStorage.getItem(indexName);
+	var panel = document.querySelector('#panel');
+	if (isNaN(index) || index >= 9) index = 0;
+	else index++;
+	// var imgUrl = imgUrls[index];
+	var imgid = index.toString();
+	// var url = "https://www.cn.bing.com" + imgUrl;
+	// panel.style.background = "url('" + url + "') center center no-repeat #666";
+
+	var url = "/assets/img/" + imgid;
+	// panel.style.background = "url('" + url + "') center center no-repeat #666";
+	if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+		// 当前设备是移动设备
+		url = url + "m.webp";
+		panel.style.background = "url('" + url + "') center center no-repeat #666";
+		panel.style.backgroundSize = "cover";
+	}
+	else	{
+		url = url + "pc.webp";
+		panel.style.background = "url('" + url + "') center center no-repeat #666";
+		panel.style.backgroundSize = "cover";
+	}
+
+	sessionStorage.setItem(indexName, index);
+}
+
+function decryptEmail(encoded) {
+	var address = atob(encoded);
+	window.location.href = "mailto:" + address;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	// 获取一言数据
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var res = JSON.parse(this.responseText);
+			document.getElementById('description').innerHTML = res.hitokoto + "<br/> -「<strong>" + res.from + "</strong>」";
+		}
+	};
+	xhr.open("GET", "https://v1.hitokoto.cn", true);
+	xhr.send();
+
+	var iUpElements = document.querySelectorAll(".iUp");
+	iUpElements.forEach(function (element) {
+		iUp.up(element);
+	});
+
+	var avatarElement = document.querySelector(".js-avatar");
+	avatarElement.addEventListener('load', function () {
+		avatarElement.classList.add("show");
+	});
+});
+
+var btnMobileMenu = document.querySelector('.btn-mobile-menu__icon');
+var navigationWrapper = document.querySelector('.navigation-wrapper');
+
+btnMobileMenu.addEventListener('click', function () {
+	if (navigationWrapper.style.display == "block") {
+		navigationWrapper.addEventListener('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+			navigationWrapper.classList.toggle('visible');
+			navigationWrapper.classList.toggle('animated');
+			navigationWrapper.classList.toggle('bounceOutUp');
+			navigationWrapper.removeEventListener('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', arguments.callee);
+		});
+		navigationWrapper.classList.toggle('animated');
+		navigationWrapper.classList.toggle('bounceInDown');
+		navigationWrapper.classList.toggle('animated');
+		navigationWrapper.classList.toggle('bounceOutUp');
+	} else {
+		navigationWrapper.classList.toggle('visible');
+		navigationWrapper.classList.toggle('animated');
+		navigationWrapper.classList.toggle('bounceInDown');
+	}
+	btnMobileMenu.classList.toggle('social');
+	btnMobileMenu.classList.toggle('iconfont');
+	btnMobileMenu.classList.toggle('icon-list');
+	btnMobileMenu.classList.toggle('social');
+	btnMobileMenu.classList.toggle('iconfont');
+	btnMobileMenu.classList.toggle('icon-angleup');
+	btnMobileMenu.classList.toggle('animated');
+	btnMobileMenu.classList.toggle('fadeIn');
+});
---
+++ getimage.py
@@ -0,0 +1,65 @@
+import time
+from io import BytesIO
+
+import requests
+from PIL import Image
+
+
+def download_image(api_url, save_dir, filename, session, agent, quality, max_size=(2560, 1440)):
+    headers = {
+        "User-Agent": agent
+    }
+    # 发送GET请求
+    try:
+        response = session.get(api_url, headers=headers, timeout=50)
+        # 检查响应状态码
+        if response.status_code != 200:
+            raise Exception("Failed to download image")
+
+        # 保存图片
+        image = Image.open(BytesIO(response.content))
+        # 将图片转换为RGB模式（如果需要）
+        if image.mode != 'RGB':
+            image = image.convert('RGB')
+
+        output = BytesIO()
+
+        image.thumbnail(max_size, Image.LANCZOS)
+        image.save(output, 'WEBP', quality=quality)
+        output.seek(0)
+
+        # 将处理后的图片保存到文件
+        save_path = save_dir + '/' + filename
+        with open(save_path, 'wb') as f:
+            f.write(output.read())
+        print(f"图片已保存到 {save_path}")
+
+    except requests.Timeout:
+        print("Request timed out")
+    except requests.RequestException as e:
+        print("An error occurred:", e)
+
+
+if __name__ == "__main__":
+    api_url_pc = "https://t.alcy.cc/fj"
+    api_url_m = "https://t.alcy.cc/aimp"
+    # save_dir = "./img"  # 替换为你的保存目录
+    save_dir = "./assets/img"  # 替换为你的保存目录
+
+    # 设置移动端的User-Agent
+    mobile_user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"
+    # 设置PC端的User-Agent
+    chrome_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+
+    # 创建一个Session对象
+    session = requests.Session()
+
+    for index in range(10):
+        filename = index.__str__() + 'm.webp'
+        download_image(api_url_m, save_dir, filename, session, mobile_user_agent, 65, max_size=(2560, 1440))
+        time.sleep(1)
+
+    for index in range(10):
+        filename = index.__str__() + 'pc.webp'
+        download_image(api_url_pc, save_dir, filename, session, chrome_user_agent, 75, max_size=(2560, 1440))
+        time.sleep(1)
